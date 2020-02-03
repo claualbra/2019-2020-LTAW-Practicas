6b501fa6ca67553b4d09d6ce53f1ff8c31ab9db7
Claudia Alvarez Bravo <claualbr@f-l3208-pc21.aulas.etsit.urjc.es>
2020-02-03 12:46:47 +0100

    empezando el html

--- P1/server.js
+++ P1/server.js
@@ -9,25 +9,15 @@ console.log("Arrancando servidor...")
 //-- Funcion para atender a una Peticion
 //-- req: Mensaje de solicitud
 //-- res: Mensaje de respuesta
-function peticion(req, res) {
-
+//-- Configurar el servidor
+http.createServer( (req, res) => {
   //-- Peticion recibida
   console.log("Peticion recibida!")
 
   //-- Crear mensaje de respuesta
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-
-}
-
-//-- Inicializar el servidor
-//-- Cada vez que recibe una petición
-//-- invoca a la funcion peticion para atenderla
-const server = http.createServer(peticion)
-
-//-- Configurar el servidor para escuchar en el
-//-- puerto establecido
-server.listen(PUERTO);
+}).listen(PUERTO);
 
 console.log("Servidor LISTO!")
 console.log("Escuchando en puerto: " + PUERTO)
